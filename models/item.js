57e89b6504f41adc54233fa745e2c3ac98bf5510
Samuel Duncan <samuel.ira.duncan@gmail.com>
2024-04-10 18:34:50 -0400

    Add validation messages to Item schema properties

--- models/item.js
+++ models/item.js
@@ -9,13 +9,20 @@ const ImageSchema = new Schema({
 
 const ItemSchema = new Schema({
   name: {
-    type: String, minLength: 3, maxLength: 50, required: [true, 'Item name is required.'],
+    type: String,
+    minLength: [3, 'Item name must be 3 or more characters.'],
+    maxLength: [50, 'Item name must not exceed 50 characters.'],
+    required: [true, 'Item name is required.'],
   },
   description: {
     type: String, minLength: 50, maxLength: 280,
   },
   price: { type: Number, required: [true, 'Item price is required.'] },
-  quantity: { type: Number, min: [1, 'Item quantity must be 1 or more.'], required: [true, 'Item quantity is required.'] },
+  quantity: {
+    type: Number,
+    min: [1, 'Item quantity must be 1 or more.'],
+    required: [true, 'Item quantity is required.'],
+  },
   images: [ImageSchema],
   category: [{ type: Schema.ObjectId, ref: 'Category' }],
 });
