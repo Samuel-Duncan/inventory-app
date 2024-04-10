8c035a6abcf46a94f8b6730d947f2c60ad3296e5
Samuel Duncan <samuel.ira.duncan@gmail.com>
2024-04-10 18:28:58 -0400

    Add Mongoose schemas for Items and Categories
    - Implemented CategorySchema to define the structure for item categories.
    - Defined properties for category name (required) and optional description.
    - Added a virtual URL property for each category instance.
    - Implemented ItemSchema to define the structure for items.
    - Defined properties for item name (required with validation), description,
      price (required), quantity (required with validation), images (array of
      image URLs), and category (reference to a Category document).
    - Added a virtual URL property for each item instance.

--- models/category.js
+++ models/category.js
@@ -1,1 +1,16 @@
 const mongoose = require('mongoose');
+
+// eslint-disable-next-line prefer-destructuring
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+  name: { type: String, required: true },
+  description: String,
+});
+
+// Virtual for this category instance URL.
+CategorySchema.virtual('url').get(function () {
+  return `/category/${this._id}`;
+});
+
+module.exports = mongoose.model('Category', CategorySchema);
--- models/item.js
+++ models/item.js
@@ -1,1 +1,28 @@
 const mongoose = require('mongoose');
+
+// eslint-disable-next-line prefer-destructuring
+const Schema = mongoose.Schema;
+
+const ImageSchema = new Schema({
+  url: String,
+});
+
+const ItemSchema = new Schema({
+  name: {
+    type: String, minLength: 3, maxLength: 50, required: [true, 'Item name is required.'],
+  },
+  description: {
+    type: String, minLength: 50, maxLength: 280,
+  },
+  price: { type: Number, required: [true, 'Item price is required.'] },
+  quantity: { type: Number, min: [1, 'Item quantity must be 1 or more.'], required: [true, 'Item quantity is required.'] },
+  images: [ImageSchema],
+  category: [{ type: Schema.ObjectId, ref: 'Category' }],
+});
+
+// Virtual for this item instance URL.
+ItemSchema.virtual('url').get(function () {
+  return `/item/${this._id}`;
+});
+
+module.exports = mongoose.model('Item', ItemSchema);
