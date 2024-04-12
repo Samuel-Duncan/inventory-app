a4fdb2d74c212ed14b2faed2230d6e427509e04c
Samuel Duncan <samuel.ira.duncan@gmail.com>
2024-04-12 17:05:31 -0400

    Set up routes and route skeleton functions

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const mongoose = require('mongoose');
 // Require routers
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const inventoryRouter = require('./routes/inventory');
 
 // Set up mongoose connection
 mongoose.set('strictQuery', false);
@@ -34,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/inventory', inventoryRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
---
+++ routes/inventory.js
@@ -0,0 +1,65 @@
+const express = require('express');
+
+const router = express.Router();
+
+const categoryController = require('../controllers/categoryController');
+const itemController = require('../controllers/itemController');
+
+/* GET inventory page. */
+router.get('/', (req, res, next) => {
+  res.render('inventory', { title: 'Whole Inventory' });
+});
+
+// CATEGORY ROUTES //
+// GET request for creating a Category.
+// NOTE This must come before routes that display Category (uses id).
+router.get('/category/create', categoryController.category_create_get);
+
+// POST request for creating Book.
+router.get('/category/create', categoryController.category_create_post);
+
+// GET request to delete Category
+router.get('/category/:id/delete', categoryController.category_delete_get);
+
+// POST request to delete Category
+router.get('/category/:id/delete', categoryController.category_delete_post);
+
+// GET request to update Category
+router.get('/category/:id/update', categoryController.category_update_get);
+
+// POST request to update Category.
+router.post('/category/:id/update', categoryController.category_update_post);
+
+// GET request for one category
+router.get('/category/:id');
+
+// GET request for list of all Category items
+router.get('/categories', categoryController.category_list);
+
+// ITEM ROUTES //
+// GET request for creating an Item.
+// NOTE This must come before routes that display Item (uses id).
+router.get('/item/create', itemController.item_create_get); // Assuming your controller is named itemController
+
+// POST request for creating Item.
+router.post('/item/create', itemController.item_create_post); // Changed GET to POST for creating an item
+
+// GET request to delete Item
+router.get('/item/:id/delete', itemController.item_delete_get);
+
+// POST request to delete Item
+router.get('/item/:id/delete', itemController.item_delete_post);
+
+// GET request to update Item
+router.get('/item/:id/update', itemController.item_update_get);
+
+// POST request to update Item.
+router.post('/item/:id/update', itemController.item_update_post); // Changed GET to POST for updating an item
+
+// GET request for one item
+router.get('/item/:id');
+
+// GET request for list of all Items
+router.get('/items', itemController.item_list); // Changed categories to items
+
+module.exports = router;
