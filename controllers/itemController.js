bb525c0210cfcae3b82addd017ea356fc8dffd29
Samuel Duncan <samuel.ira.duncan@gmail.com>
2024-04-20 15:51:15 -0400

    Fix issue where new items being created were not saving their selected categories

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,5 +1,8 @@
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
+
 const Item = require('../models/item');
+const Category = require('../models/category');
 
 // Display list of all Items
 exports.item_list = asyncHandler(async (req, res, next) => {
@@ -30,13 +33,83 @@ exports.item_detail = asyncHandler(async (req, res, next) => {
 
 // Display Item create form on GET.
 exports.item_create_get = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Item create GET');
+  const allCategories = await Category.find().sort({ name: 1 }).exec();
+  res.render('item_form', {
+    title: 'Create Game',
+    categories: allCategories,
+  });
 });
 
 // Handle Item create on POST.
-exports.item_create_post = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Item create POST');
-});
+exports.item_create_post = [
+  // Convert the categories to an array
+  (req, res, next) => {
+    if (!Array.isArray(req.body.category)) {
+      req.body.categories = typeof req.body.categories === 'undefined' ? [] : [req.body.category];
+    }
+    next();
+  },
+
+  // Validate and sanitize fields
+  body('name')
+    .trim() // Trim leading/trailing whitespace
+    .notEmpty()
+    .withMessage('Item name is required.')
+    .isLength({ min: 3, max: 50 })
+    .withMessage('Item name must be between 3 and 50 characters.')
+    .escape(), // Basic sanitization to prevent XSS,
+  body('description')
+    .trim()
+    .notEmpty()
+    .withMessage('Item description is required.')
+    .isLength({ min: 20, max: 1000 })
+    .withMessage('Description must be between 20 and 1000 characters.')
+    .escape(), // Consider extending max length if needed,
+  body('price')
+    .isNumeric()
+    .withMessage('Item price must be a number.')
+    .notEmpty()
+    .withMessage('Item price is required.')
+    .custom((value) => value >= 0 || 'Item price must be non-negative'),
+  body('quantity')
+    .isInt({ min: 1 })
+    .withMessage('Item quantity must be a whole number (minimum 1).')
+    .notEmpty()
+    .withMessage('Item quantity is required.'),
+  body('categories.*').escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    // Create an Item object with escaped and trimmed data.
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      quantity: req.body.quantity,
+      categories: req.body.category,
+    });
+
+    if (!errors.isEmpty()) {
+      const allCategories = await Category.find().sort({ name: 1 }).exec();
+
+      for (const category of allCategories) {
+        if (item.categories.includes(category._id)) {
+          category.checked = 'true';
+        }
+      }
+      res.render('item_form', {
+        title: 'Create Game',
+        categories: allCategories,
+        item,
+        errors: errors.array(),
+      });
+    } else {
+      await item.save();
+      res.redirect(item.url);
+    }
+  }),
+];
 
 // Display Item delete form on GET.
 exports.item_delete_get = asyncHandler(async (req, res, next) => {
